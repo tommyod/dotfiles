d82da0a62b4ce41a0db1a66ef258b985bc321d15
Gunvor Lemvik <gunvor.lemvik@gmail.com>
2020-03-22 12:05:19 +0100

    legg til Gunvor sine filer

---
+++ Gunvor/setup_mac.sh
@@ -0,0 +1,9 @@
+# Make sure we are using the latest HomeBrew
+brew update
+
+# Upgrade any already installed formulae
+brew upgrade
+
+# Nice to have
+brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" dropbox
